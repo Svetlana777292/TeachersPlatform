b167de686cfa8a5c630dbe36e9ce4170cf5ac89f
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-04-05 10:07:45 +0300

    checking token after opening app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,47 @@ import {Route, Routes} from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
+import {useEffect, useRef, useState} from "react";
+import checkToken from "./utils/checkToken.js";
+import Loading from "./components/Loading.jsx";
 
 function App() {
+  const [token, setToken] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const hasChecked = useRef(false)
+
+  useEffect(() => {
+    if(hasChecked.current) return
+
+    hasChecked.current = true
+
+    async function verifyToken() {
+        try{
+            const result = await checkToken()
+            setToken(result)
+        }
+        catch(error){
+            console.log(error)
+            setToken(false)
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    verifyToken()
+  }, [])
+
+  if(loading){
+      return <Loading />
+  }
 
   return (
     <>
       <Routes>
         <Route path="/register/" element={<RegisterPage/>}/>
         <Route path="/login/" element={<LoginPage/>}/>
-        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/" element={token ? <ProfilePage /> : <LoginPage/>}/>
         <Route path="/Profile/" element={<ProfilePage/>}/>
       </Routes>
     </>
