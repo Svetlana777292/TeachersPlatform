f3ac0891bc5ef2a455c538283c7bd7d171261fad
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-04-27 21:10:25 +0300

    fixTimezone.js deleted

--- src/utils/fixTimezone.js
+++
@@ -1,7 +0,0 @@
-const fixTimezone = (dateString) => {
-    const date = new Date(dateString)
-    date.setMinutes(date.getMinutes() + date.getTimezoneOffset())
-    return date
-}
-
-export default fixTimezone
