98358ce83055452f608895849ee0b1e63fa8111d
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-05-02 11:09:38 +0300

    hover button resizing bug fixed

--- src/components/AddStudentModal/AddStudentModal.jsx
+++ src/components/AddStudentModal/AddStudentModal.jsx
@@ -8,7 +8,7 @@ const AddStudentModal = (props) => {
         <Modal className="modalWindow addStudentModal" onRequestClose={props.onClose} isOpen={props.isOpen} >
             <h1 className="addStudentPrompt">Do you want to add this student?</h1>
             <div className="addButtons">
-                <Button type="button" onClick={() => props.onClose()}>No</Button>
+                <Button type="button" className="disagreeButton" onClick={() => props.onClose()}>No</Button>
                 <Button type="submit" className="agreeButton" onClick={(e) => {
                     handleSubmit("POST", props.student, "me/add_student", e)
                     props.onClose()
