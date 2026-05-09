0d3a25aafc7450d650ccb52a78b5a967274bc7eb
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-05-09 14:17:53 +0300

    logout with rtk

--- src/components/AccountManagement/AccountManagement.jsx
+++ src/components/AccountManagement/AccountManagement.jsx
@@ -4,10 +4,12 @@ import Button from "../Button/Button.jsx";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 import "./AccountManagement.css"
+import {useLogoutUserMutation} from "../../store/api/userApi.js";
 
 const AccountManagement = () => {
     const navigate = useNavigate()
     const [editingPasswordEnabled, setEditingPasswordEnabled] = useState(false)
+    const [logoutUser, {error}] = useLogoutUserMutation()
 
     async function handleLogout(e, navigate){
         e.preventDefault()
@@ -35,7 +37,11 @@ const AccountManagement = () => {
             <div className="action-btns-container">
                 <h3 className="action-title">Account management</h3>
                 <Button type="button" className="btn exit-btn" onClick={() => setEditingPasswordEnabled(true)}>Change password</Button>
-                <Button type="button" className="btn exit-btn" onClick={(e) => handleLogout(e, navigate)}>Exit</Button>
+                <Button type="button" className="btn exit-btn" onClick={(e) => {
+                    e.preventDefault()
+                    logoutUser()
+                    navigate('/login')
+                }}>Exit</Button>
                 <Button type="button" className="btn delete-btn" >Delete profile</Button>
             </div>
 
