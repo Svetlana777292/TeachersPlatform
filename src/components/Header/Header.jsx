bcb08761622169764d64e66ec709ee759907e5a9
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-06-11 12:12:41 +0300

    edited header position bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,16 +10,18 @@ const Header = ({currentPage}) => {
 
     return(
         <header className="main-header">
-            <h1 className="page-title">{user?.role === "teacher" ? "Teacher dashboard" : "Student dashboard"}</h1>
-            <Button className="burger-btn" type="button" onClick={() => setMenuOpened(!menuOpened)}>
-                <svg className={`burger-icon ${menuOpened ? "rotated" : ""}`} viewBox="0 0 24 24" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 6H20M4 12H20M4 18H20" stroke="black" strokeWidth="2" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                </svg>
-            </Button>
+            <div className="layout-container header-inner">
+                <h1 className="page-title">{user?.role === "teacher" ? "Teacher dashboard" : "Student dashboard"}</h1>
+                <Button className="burger-btn" type="button" onClick={() => setMenuOpened(!menuOpened)}>
+                    <svg className={`burger-icon ${menuOpened ? "rotated" : ""}`} viewBox="0 0 24 24" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 6H20M4 12H20M4 18H20" stroke="black" strokeWidth="2" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                    </svg>
+                </Button>
 
-            <NavBar currentPage={currentPage} role={user?.role} className={`menu-list ${menuOpened ? "is-open" : ""}`}/>
+                <NavBar currentPage={currentPage} role={user?.role} className={`menu-list ${menuOpened ? "is-open" : ""}`}/>
+            </div>
         </header>
     )
 }
