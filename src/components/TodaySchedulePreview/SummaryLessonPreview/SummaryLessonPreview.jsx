819af91b700ae4111f5620ee8b48f3c21c21a6e9
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-06-15 22:11:16 +0300

    today's schedule in profile redesigned

---
+++ src/components/TodaySchedulePreview/SummaryLessonPreview/SummaryLessonPreview.jsx
@@ -0,0 +1,30 @@
+import getNameById from "../../../utils/getName.js";
+import {getEndTimeString, getTimeString} from "../../../utils/getEndTimeString.js";
+import {useMyStudents} from "../../../hooks/useMyStudents.js";
+import {useMyTeachers} from "../../../hooks/useMyTeachers.js";
+import {useGetUserQuery} from "../../../store/api/userApi.js";
+import "./SummaryLessonPreview.css"
+import BookIcon from "../../../assets/BookIcon.jsx";
+
+const SummaryLessonPreview = ({lesson}) => {
+    const {myStudents} = useMyStudents()
+    const {myTeachers} = useMyTeachers()
+    const {data: user} = useGetUserQuery()
+    const people = user.role === 'teacher' ? myStudents : myTeachers
+
+    return (
+        <>
+            <div className="todayLessonCard" style={{borderLeft: `5px solid ${lesson.card_color}`}}>
+                <div className="iconWrapper" style={{background: lesson.card_color}}>
+                    <BookIcon className="bookIcon"/>
+                </div>
+                <div className="lessonTopic">{lesson.topic}</div>
+                <div className="previewName">{getNameById(lesson.student_id, people)}</div>
+                <div className="lessonTimePeriod">{`${getTimeString(lesson.date)} - ${getEndTimeString(lesson.date, lesson.duration)}`}</div>
+                <div className="lessonDuration">{`${lesson.duration} min`}</div>
+            </div>
+        </>
+    )
+}
+
+export default SummaryLessonPreview
