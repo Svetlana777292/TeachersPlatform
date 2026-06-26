d7419cd3646bae9095dc2e18ea9bdd9d36c0452a
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-06-26 19:39:05 +0300

    new icons

---
+++ src/assets/Mastercard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const MastercardIcon = ({ className = "w-6 h-6" }) => (
+    <svg className={className} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Mastercard</title><g fill="none" fillRule="evenodd"><circle cx="7" cy="12" r="7" fill="#EA001B"/><circle cx="17" cy="12" r="7" fill="#FFA200" fillOpacity=".8"/></g></svg>
+)
