b0bb8bd100f14837bda823203867d6819cad4454
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-06-28 16:07:08 +0300

    fixed ignoring accidental tokenizations

--- src/components/TransactionsHistory/useTransactionHistory.jsx
+++ src/components/TransactionsHistory/useTransactionHistory.jsx
@@ -7,9 +7,15 @@ function useTransactionHistory() {
     const transactionsByDate = useMemo(() => {
         const grouped = {}
         for (const transaction of transactions) {
-            const dateKey = transaction.created_at.slice(0, 10)
-            if (!grouped[dateKey]) grouped[dateKey] = []
-            grouped[dateKey].push(transaction)
+            console.log(JSON.stringify(transaction.type));
+            if (transaction.type === "TOKENIZATION" && transaction.status === "PENDING") {
+                continue
+            }
+            else {
+                const dateKey = transaction.created_at.slice(0, 10)
+                if (!grouped[dateKey]) grouped[dateKey] = []
+                grouped[dateKey].push(transaction)
+            }
         }
         return grouped
     }, [transactions])
