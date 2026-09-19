90ca4cd4c3fad07fc8fa5e637804e69e461756cd
Sviatlana Aniskavets <vestaaaaaaaaaf@gmail.com>
2026-09-19 19:35:27 +0300

    feat: add React.lazy and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import {lazy, Suspense} from "react";
 import {Route, Routes, Navigate} from "react-router-dom";
-import RegisterPage from "./pages/Auth/RegisterPage/RegisterPage.jsx";
-import LoginPage from "./pages/Auth/LoginPage.jsx";
-import ProfilePage from "./pages/Profile/ProfilePage.jsx";
+const RegisterPage = lazy(() => import("./pages/Auth/RegisterPage/RegisterPage.jsx"));
+const LoginPage = lazy(() => import("./pages/Auth/LoginPage.jsx"));
 import Loading from "./components/Loading/Loading.jsx";
-import SchedulePage from "./pages/SchedulePage/SchedulePage.jsx";
+const ProfilePage = lazy(() => import("./pages/Profile/ProfilePage.jsx"));
+const SchedulePage = lazy(() => import("./pages/SchedulePage/SchedulePage.jsx"));
 import {useVerifyUserQuery} from "./store/api/userApi.js";
-import PeoplePage from "./pages/People/PeoplePage/PeoplePage.jsx";
+const PeoplePage = lazy(() => import ("./pages/People/PeoplePage/PeoplePage.jsx"))
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./utils/toastStyles.css"
-import FinancePage from "./pages/FinancePage/FinancePage.jsx";
-import StudentLessonPage from "./pages/StudentLessonsPage/StudentLessonPage.jsx";
+const FinancePage = lazy(() => import("./pages/FinancePage/FinancePage.jsx"));
+const StudentLessonPage = lazy(() => import ("./pages/StudentLessonsPage/StudentLessonPage.jsx"))
 
 function App() {
   const {isSuccess: isTokenValid, isLoading} = useVerifyUserQuery()
@@ -23,20 +24,22 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/register/" element={<RegisterPage/>}/>
-        <Route path="/login/" element={<LoginPage/>}/>
-        <Route path="/" element={isTokenValid ? <ProfilePage /> : <LoginPage/>}/>
-        <Route path="/profile/" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
-        <Route path="/people/" element={<ProtectedRoute><PeoplePage /></ProtectedRoute>}/>
-        <Route path="/schedule/" element={<ProtectedRoute><SchedulePage/></ProtectedRoute>}/>
-        <Route path="/finance/" element={<ProtectedRoute><FinancePage/></ProtectedRoute>}/>
-        <Route path="/lessons/" element={<ProtectedRoute><StudentLessonPage/></ProtectedRoute>}/>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/register/" element={<RegisterPage/>}/>
+          <Route path="/login/" element={<LoginPage/>}/>
+          <Route path="/" element={isTokenValid ? <ProfilePage /> : <LoginPage/>}/>
+          <Route path="/profile/" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
+          <Route path="/people/" element={<ProtectedRoute><PeoplePage /></ProtectedRoute>}/>
+          <Route path="/schedule/" element={<ProtectedRoute><SchedulePage/></ProtectedRoute>}/>
+          <Route path="/finance/" element={<ProtectedRoute><FinancePage/></ProtectedRoute>}/>
+          <Route path="/lessons/" element={<ProtectedRoute><StudentLessonPage/></ProtectedRoute>}/>
 
-        <Route path="/payments/*" element={<PaymentRedirect />} />
+          <Route path="/payments/*" element={<PaymentRedirect />} />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
       <ToastContainer autoClose={3000} position={"top-center"} toastClassName="appToast" className="appToastContainer" hideProgressBar={true}/>
     </>
   )
